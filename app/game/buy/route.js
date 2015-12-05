5b5ef26c699259171bc56c6eb7c1e9e7cda4e7e6
Jeremy Glover <jeremyglover33@gmail.com>
2015-12-05 17:16:55 -0600

    Shows buy popup

--- app/game/buy/route.js
+++ app/game/buy/route.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        id: 1,
+        business: 'Apartment',
+        cost: '$2,000',
+        needed: 0,
+        earnings: 0,
+      },
+      {
+        id: 2,
+        business: 'Coffee Shop',
+        cost: '$2,000',
+        needed: 3,
+        earnings: '$1,000',
+      },
+      {
+        id: 3,
+        business: 'Music Studio',
+        cost: '$4,000',
+        needed: 3,
+        earnings: '$2,000',
+      },
+    ];
+  },
+
+  actions: {
+    goBack() {
+      this.transitionTo('game.index');
+    },
+  },
 });
