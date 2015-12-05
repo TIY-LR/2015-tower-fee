8dedae62cf1193e08e87816b8a0b787c89d3e102
Chancey Ward <chancey_ward@yahoo.com>
2015-12-05 11:20:19 -0600

    Setup routes

---
+++ tests/unit/game/buy/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:game/buy', 'Unit | Route | game/buy', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
