14852c142906ecd5497abd46302dc8b0b787a9e0
Chancey Ward <chancey_ward@yahoo.com>
2015-12-06 13:07:19 -0600

    Use accounting to show game money

---
+++ tests/unit/helpers/round-down-test.js
@@ -0,0 +1,10 @@
+import { roundDown } from '../../../helpers/round-down';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | round down');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = roundDown(42);
+  assert.ok(result);
+});
