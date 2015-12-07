1991ad3c178900cb6c1d019993eedcdba7de7fca
Jeremy Glover <jeremyglover33@gmail.com>
2015-12-06 19:46:22 -0600

    old changes

--- app/app.js
+++ app/app.js
@@ -16,3 +16,20 @@ App = Ember.Application.extend({
 loadInitializers(App, config.modulePrefix);
 
 export default App;
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+   if (i < 10) {i = "0" + i};
+   return i;
+}
